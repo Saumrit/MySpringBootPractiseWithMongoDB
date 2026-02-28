2c9ab285f7351421ab7ebe70f6c9fdb6282a2c8d
Saumrit <44732905+Saumrit@users.noreply.github.com>
2026-02-28 11:55:15 +0530

    My first commit with Local setUp

---
+++ src/test/java/com/saumrit/MySpringBootPractiseWithMongoDB/MySpringBootPractiseWithMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saumrit.MySpringBootPractiseWithMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBootPractiseWithMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
